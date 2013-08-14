9489ee58e200a8ae9ddaed95ee7739d37d59ffce
Filipe Borges <fqborges@gmail.com>
2013-08-14 12:55:15 -0300

    ok

--- teste.js
+++ teste.js
@@ -1,13 +1,65 @@
 //--------------------
 /**
+ * Entity
+ **/
+var Entity = function(id, entityManager){
+  this._em = entityManager;
+  this.id = id;
+  
+  this._components = {};
+};
+
+/**
+ * add( {string}, {object} )
+ **/
+Entity.prototype.add = function(name, component){
+  this._components[name] =  component;
+};
+
+/**
+ * get( {string} ) -> {object}
+ **/
+Entity.prototype.get = function(name){
+  return this._components[name];
+};
+
+/**
+ * has( {string} ) -> {boolean}
+ **/
+Entity.prototype.has = function(name){
+  return !!this._components[name];
+};
+
+/**
+ * toString() -> {string}
+ **/
+Entity.prototype.toString = function(){
+  return '[Entity id=' + this.id + ']';
+};
+
+//--------------------
+/**
  * IdentifierPool
  **/
 var IdentifierPool = function(){
+  this._ids = [];
   this._nextID = 0;
 };
 
+/**
+ * get() -> {int}
+ **/
 IdentifierPool.prototype.get = function(){
-  return this._nextID++;
+  return this._ids.length > 0 ?
+         this._ids.pop() :
+         this._nextID++;
+}
+
+/**
+ * giveBack( {int} )
+ **/
+IdentifierPool.prototype.giveBack = function(id){
+  this._ids.push(id);
 }
 
 //--------------------
@@ -19,22 +71,10 @@ var EntityManager = function(){
 };
 
 /**
- * createEntity -> Entity
+ * createEntity() -> {Entity}
  **/
 EntityManager.prototype.createEntity = function(){
-  return new Entity( this._idPool.get() );
-};
-
-//--------------------
-/**
- * Entity
- **/
-var Entity = function(id){
-  this.id = id;
-};
-
-Entity.prototype.toString = function(){
-  return '[Entity id=' + this.id + ']';
+  return new Entity(this._idPool.get(), this);
 };
 
 //--------------------
@@ -42,15 +82,41 @@ Entity.prototype.toString = function(){
  * World
  **/
 var World = function(){
+  this._systems = [];
+  this._ents = [];
   this._em = new EntityManager();
 };
 
 /**
- * createEntity -> Entity
+ * createEntity -> {Entity}
  **/
 World.prototype.createEntity = function(){
   return this._em.createEntity();
 };
+
+/**
+ * addEntity( {Entity} )
+ **/
+World.prototype.addEntity = function(entity){
+  return this._ents.push(entity);
+};
+
+/**
+ * step( {number} )
+ **/
+World.prototype.step = function(delta) {
+  var systems = this._systems;
+  for(var i=0, sys; sys=systems[i]; i++ ) {
+    sys.step && sys.step(delta, this._ents);
+  }
+}
+
+/**
+ * addSystem( {object} )
+ **/
+World.prototype.addSystem = function(system) {
+  this._systems.push(system);
+}
 
 //--------------------
 /**
@@ -59,8 +125,37 @@ World.prototype.createEntity = function(){
 
 window.addEventListener('load', function() {
     var world = new World();
-    var entity = world.createEntity();
-    alert(entity.toString());
     
+    world.addSystem( {
+      acceptEntity : function(ent) {
+        return ent.has('velocidade') && ent.has('posicao');
+      },
+      step : function(delta, entities) {
+        var dt = delta / 1000;
+        for (var i=0, ent; ent=entities[i]; i++) {
+          if(!this.acceptEntity(ent)) continue;
+          
+          var v = ent.get('velocidade');
+          var p = ent.get('posicao');
+          
+          p.x = p.x + v.x * dt;
+          p.y = p.y + v.y * dt;
+        }
+      }
+    } );
+    
+    var entity = world.createEntity();
+    entity.add('velocidade', { x:1, y: 1} );
+    entity.add('posicao', {x:0, y:0} );
+    world.addEntity(entity);
+    
+    var steps = [200, 300, 500];
+    
+    for( var i=0, step; step = steps[i]; i++ ) {
+      world.step(step);
+      
+      var p = entity.get('posicao');
+      alert('[pos x=' + p.x + ', y=' + p.y + ']');
+    }
     
 });
